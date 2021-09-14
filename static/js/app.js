47aded015a2bacd2c9d4e232b574344b9e3f8ae9
JerryMcG <76828978+JerryMcG@users.noreply.github.com>
2021-09-13 22:23:33 -0400

    adding app.js file

---
+++ static/js/app.js
@@ -0,0 +1,23 @@
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+// code to build out table
+function buildTable(data) {
+    //clear any existing data in table
+    tbody.html("");
+    // loop through each object in the data 
+    //& append row and cells for each value in row
+    data.forEach((datarow) => {
+        //add row to table body
+        let row = tbody.append("tr");
+        //loop through each field in dataRow and add
+        //each value as a table cell (td)
+        Object.values(dataRow).forEach((val) => {
+            let cell = row.append("td");
+            cell.text(val);
+        });
+    });
+}
